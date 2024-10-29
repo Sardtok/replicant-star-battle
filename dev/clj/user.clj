171550813023b7be5f5fcadeba3c5115c5e326bb
Sigmund Hansen <github@chickensoft.com>
2024-10-30 00:56:17 +0100

    Add a function that can be used as a pre-commit hook to build CLJS
    The pre-commit hook checks if CLJS or CLJC files have changed,
    builds the CLJS with shadow and adds the compiled JS to the commit.

--- dev/clj/user.clj
+++ dev/clj/user.clj
@@ -19,3 +19,23 @@
        (start-server!)
        (watch build-id)
        (nrepl-select build-id)))))
+
+(defn build-cljs-if-changed
+  "Builds a release build of the CLJS code and adds it to the commit
+   if any CLJS files have been changed in the commit."
+  [& args]
+  (let [git-staged-proc (proc/start "git" "diff" "--cached" "--name-only")
+        git-staged-cljs (-> (proc/stdout git-staged-proc)
+                            slurp
+                            (s/split #"\R")
+                            (->> (filter #(re-find #"\.clj[sc]$" %))))]
+    (when (seq git-staged-cljs)
+      (println "CLJS has changed. Building release and adding to commit.")
+      (try
+        (require-shadow)
+        (catch Exception _
+          (.println *err* "Could not load shadow. Run with clj -X:dev:shadow user/build-cljs-if-changed")
+          (System/exit 1)))
+      (let [release (resolve 'shadow/release)]
+        (release :app)
+        @(proc/exit-ref (proc/start "git" "add" "resources/js/main.js"))))))
