04703f4cc4591c84fe06bfd606ac58504d515e45
Sigmund Hansen <github@chickensoft.com>
2024-10-30 01:27:48 +0100

    Use standard ring middleware to serve files and some extras
    Serve files using standard middleware,
    make sure / gets the appropriate content type,
    use standard middleware for other request content types,
    use standard middleware for modified since.

--- src/clj/chickensoft/star_battle.clj
+++ src/clj/chickensoft/star_battle.clj
@@ -1,13 +1,31 @@
 (ns chickensoft.star-battle
   (:require [clojure.string :as s]
             [org.httpkit.server :as server]
-            [ring.util.response :as response]
-            [shadow.cljs.devtools.api :as shadow]))
+            [ring.middleware.content-type :as content-type-mw]
+            [ring.middleware.file :as file-mw]
+            [ring.middleware.not-modified :as not-modified-mw]
+            [ring.util.response :as ring-response])
+  (:import (java.io File)))
 
-(defn handler [req]
-  (let [path (subs (:uri req) 1)
-        path (if (s/blank? path) "index.html" path)]
-    (response/file-response path {:root "resources"})))
+(defn- add-content-type-if-index-html [response]
+  (if (and (instance? File (:body response))
+           (-> response :body .getName (s/ends-with? "index.html")))
+    (ring-response/content-type response "text/html")
+    response))
+
+(defn wrap-index-html-content-type
+  [handler]
+  (fn
+    ([request] (-> request handler add-content-type-if-index-html))
+    ([request respond raise]
+     (-> request (handler (fn [response] (-> response add-content-type-if-index-html respond)) raise)))))
+
+(def handler
+  (-> (fn api-handler [req] nil)
+      (file-mw/wrap-file "resources")
+      wrap-index-html-content-type
+      content-type-mw/wrap-content-type
+      not-modified-mw/wrap-not-modified))
 
 (defn start-server! []
   (server/run-server #'handler {:legacy-return-value? false
