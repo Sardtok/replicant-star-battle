f8f7812423f915e66f6da508203feb4d11b4dd80
Sigmund Hansen <github@chickensoft.com>
2024-11-14 00:06:04 +0100

    Add a test grid
    This adds markup and CSS for testing a 5x5 grid.
    Colors aren't great, but they look like they hold up in color blindness simulators.
    Gray scale doesn't have enough contrast, and probably requires a separate theme.
    
    Not sure if the cell corner patterns are working.
    They were meant to make regions identifiable by a non-color marker.

--- src/cljs/chickensoft/star_battle/core.cljs
+++ src/cljs/chickensoft/star_battle/core.cljs
@@ -1,9 +1,40 @@
 (ns chickensoft.star-battle.core
-      (:require [replicant.dom :as dom]))
+  (:require [replicant.dom :as dom]))
 
 (defn ^export run []
       (dom/render
-            (.getElementById js/document "root")
-            [:div
-             [:h1 "Star Battle"]
-             [:div.game]]))
+        (.getElementById js/document "root")
+        [:div
+         [:h1 "Star Battle"]
+         [:div.game
+          [:div.grid.grid-5x5
+           [:div.row
+            [:div.cell.region-1]
+            [:div.cell.region-2]
+            [:div.cell.region-3]
+            [:div.cell.region-3]
+            [:div.cell.region-3]]
+           [:div.row
+            [:div.cell.region-1]
+            [:div.cell.region-2]
+            [:div.cell.region-2]
+            [:div.cell.region-4]
+            [:div.cell.region-5]]
+           [:div.row
+            [:div.cell.region-1]
+            [:div.cell.region-1]
+            [:div.cell.region-2]
+            [:div.cell.region-4]
+            [:div.cell.region-5]]
+           [:div.row
+            [:div.cell.region-1]
+            [:div.cell.region-4]
+            [:div.cell.region-4]
+            [:div.cell.region-4]
+            [:div.cell.region-5]]
+           [:div.row
+            [:div.cell.region-1]
+            [:div.cell.region-1]
+            [:div.cell.region-5]
+            [:div.cell.region-5]
+            [:div.cell.region-5]]]]]))
