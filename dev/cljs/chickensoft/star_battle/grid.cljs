9de934e68270b8a7e7d0c9e3e9c6937ae7896a05
Sigmund Hansen <github@chickensoft.com>
2024-12-11 21:05:56 +0100

    Testing with larger grids in the component portfolio

--- dev/cljs/chickensoft/star_battle/grid.cljs
+++ dev/cljs/chickensoft/star_battle/grid.cljs
@@ -11,3 +11,63 @@
                                 [{:region 1 :content nil} {:region 1 :content :mark} {:region 5 :content nil} {:region 5 :content nil} {:region 5 :content nil}]]})
           [state]
           [:div.game (grid/render (sb/prepare-board (:board @state)))])
+
+(defscene grid-6x6
+          :param (atom {:board [[{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6}]]})
+          [state]
+          [:div.game (grid/render (sb/prepare-board (:board @state)))])
+
+(defscene grid-7x7
+          :param (atom {:board [[{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7}]]})
+          [state]
+          [:div.game (grid/render (sb/prepare-board (:board @state)))])
+
+(defscene grid-8x8
+          :param (atom {:board [[{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8}]]})
+          [state]
+          [:div.game (grid/render (sb/prepare-board (:board @state)))])
+
+(defscene grid-9x9
+          :param (atom {:board [[{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9}]]})
+          [state]
+          [:div.game (grid/render (sb/prepare-board (:board @state)))])
+
+(defscene grid-10x10
+          :param (atom {:board [[{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9} {:region 10}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9} {:region 10}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9} {:region 10}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9} {:region 10}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9} {:region 10}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9} {:region 10}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9} {:region 10}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9} {:region 10}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9} {:region 10}]
+                                [{:region 1} {:region 2} {:region 3} {:region 4} {:region 5} {:region 6} {:region 7} {:region 8} {:region 9} {:region 10}]]})
+          [state]
+          [:div.game (grid/render (sb/prepare-board (:board @state)))])
